a3c2ab6e8cda451b30939c97640df62cf213bdb6
hong8yung <hoyun342@gmail.com>
2019-07-15 18:00:40 +0900

    BOJ_17298_오큰수

--- BOJ/17298.py
+++ BOJ/17298.py
@@ -1,18 +1,51 @@
 import sys
 inp = sys.stdin.readline
 
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, x):
+        self.items.append(x)
+
+    def pop(self):
+        if self.is_empty():
+            return False
+        else:
+            self.items.pop()
+            return True
+    
+    def top(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return not self.items
+
 if __name__ == "__main__":
     numbers = []
-    result = []
+    stack = Stack()
+    
     N = int(inp())
     numbers = list(map(int, inp().split()))
+    result = [0 for x in range(N)]
 
-    for i, number in enumerate(numbers):
-        for j in range(i, N):
-            if numbers[i] < numbers[j]:
-                result.append(numbers[j])
-                break
-            elif j==N-1:
-                result.append(-1)
+    for idx in range(N-1, -1, -1):
+        if not stack.is_empty():
+            while stack.top() <= numbers[idx]:
+                stack.pop()
+                if stack.is_empty():
+                    break
 
-    print(result)
+        if stack.is_empty():
+            result[idx] = -1
+        else:
+            result[idx] = stack.top()
+        
+        stack.push(numbers[idx])
+
+    for i in result:
+        print(i, end=' ')
