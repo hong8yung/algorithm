beb4dc73cdb0c9124e21f07c7a94b8db2ea7daa1
hong8yung <hoyun342@gmail.com>
2019-07-18 17:30:52 +0900

    Programmers_스킬트리

---
+++ Programmers/스킬트리.py
@@ -0,0 +1,21 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for skill_tree in skill_trees:
+        tmp = skill
+        print tmp
+        for i in skill_tree:
+            if i in tmp:
+                if i==tmp[0]:
+                    tmp= tmp[1:]
+                else:
+                    break
+        else:
+            answer += 1
+            print skill_tree
+
+    return answer
+
+skill = "CBD"
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+
+print solution(skill, skill_trees)
