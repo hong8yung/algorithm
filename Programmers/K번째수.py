f4112f457480605fbdaa31b7836dd83ba5bfaf2f
hong8yung <hoyun342@gmail.com>
2019-07-18 17:30:20 +0900

    Programmers_K번째수

--- Programmers/K번째수.py
+++ Programmers/K번째수.py
@@ -9,4 +9,7 @@ def solution(array, commands):
 array = [1,5,2,6,3,7,4]
 commands = [[2,5,3], [4,4,1], [1,7,3]]
 
+def solution_Awesome(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1, commands))
+    
 print(solution(array, commands))
