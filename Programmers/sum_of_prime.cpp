909cb61d20b2d41c955ee0131227ebab0d966f6e
hong8yung <hoyun342@gmail.com>
2019-07-17 21:35:40 -0300

    Programmers_sum of prime_eratosthenes sieve

--- Programmers/sum_of_prime.cpp
+++ Programmers/sum_of_prime.cpp
@@ -1,20 +1,22 @@
 #include <vector>
-
 using namespace std;
-bool chk_prime(int x){
-    if(x<2) return false;
-    for(int i=2; i<=(int)(x/2); i++){
-        if(x%i==0) return false;
-    }
-    
-    return true;
-}
-long long solution(int N) {
+
+long long solution(int N)
+{
     long long answer = 0;
-    for(int i=2; i<=N; i++){
-        if(chk_prime(i)){
+    vector<bool> arr(N + 1);
+
+    for (int i = 2; i <= N; i++)
+    {
+        if (!arr[i])
+        {
             answer += i;
+            for (int j = i; j <= N; j += i)
+            {
+                arr[j] = true;
+            }
         }
     }
+
     return answer;
 }
