2f5f30fb09fde634c0cf6967ad6a5c9795a89e68
hong8yung <hoyun342@gmail.com>
2019-07-30 23:59:04 +0900

    Programmers_완주하지 못한 선수_fail

---
+++ Programmers/완주하지 못한 선수.js
@@ -0,0 +1,12 @@
+function solution(participant, completion){
+    var answer = '';
+    for(var i in completion){
+        participant.splice(participant.indexOf(completion[i]), 1)
+    }
+    return answer;
+}
+
+par =[["leo", "kiki", "eden"], ["marina", "josipa", "nikola", "vinko", "filipa"], ["mislav", "stanko", "mislav", "ana"]]
+compl = [["eden", "kiki"],["josipa","filipa", "marina", "nikola"], ["stanko", "ana", "mislav"]]
+
+console.log(solution(par[0], compl[0]))
