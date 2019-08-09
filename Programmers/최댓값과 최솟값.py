69a2ffdd3abc8292c2fe3b7a8eba9761df8296ad
hong8yung <hoyun342@gmail.com>
2019-08-09 20:12:29 +0900

    Programmers_최댓값과 최솟값

---
+++ Programmers/최댓값과 최솟값.py
@@ -0,0 +1,7 @@
+def solution(s):
+    tmp = [int(i) for i in s.split()]
+    return str(min(tmp))+" "+str(max(tmp))
+s = ["1 2 3 4", "-1 -2 -3 -4", "-1 -1"]
+print(solution(s[0]))
+print(solution(s[1]))
+print(solution(s[2]))
