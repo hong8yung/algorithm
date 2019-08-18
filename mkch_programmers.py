5716cb937ec558087f84ec2df37512528c537a4c
hong8yung <hoyun342@gmail.com>
2019-08-19 02:58:11 +0900

    create: 프로그래머스 플랫폼 스크립트

---
+++ mkch_programmers.py
@@ -0,0 +1,34 @@
+import argparse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = ""
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--link", help="input Programmers's Chall Link")
+    args = parser.parse_args()
+    if args.link:
+        url = args.link
+        html = urlopen(url)
+        source = html.read() # read source from byte code
+        html.close()
+
+        soup = BeautifulSoup(source, "html5lib")
+        # make [Challenge's title].py file
+        title = soup.find('li', class_='algorithm-title').text.strip()
+        arg_sec = soup.find('table', class_='table')
+        arg_name = [x.text for x in arg_sec.find_all('th')]
+        arg_val_tmp = arg_sec.find_all('td')
+        arg_val = []
+        for i in arg_val_tmp:
+            if i.find('q'):
+                arg_val.append(i.text)
+            else:
+                # True/False나 None, 배열 등일 때 처리 추가로 해야함 ㄷㄷ
+                arg_val.append(int(i.text))
+
+        print(title, arg_name, arg_val)
+        
+
+if __name__=="__main__":
+    main()
