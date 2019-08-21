bffdf5ea13a63205cc5c3a2d827f25941774b433
hong8yung <hoyun342@gmail.com>
2019-08-22 00:04:28 +0900

    Programmers_소수 찾기

--- Programmers/소수 찾기.py
+++ Programmers/소수 찾기.py
@@ -9,6 +9,15 @@ def solution(n):
     
     return tmp.count(True) - 2
 
+def solution_idle(n):
+    num = set(range(2, n+1))
+
+    for i in range(2, n+1):
+        if i in num:
+            num -= set(range(2*i, n+1, i))
+
+    return len(num)
+
 n = [20, 29]
 print(solution(n[0]))
 print(solution(n[1]))
