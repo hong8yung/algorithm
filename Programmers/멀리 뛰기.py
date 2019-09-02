c2515f08c62a7bb956e1ab595cc6f069c655f666
hong8yung <hoyun342@gmail.com>
2019-09-03 04:24:17 +0900

    Programmers_멀리뛰기

--- Programmers/멀리 뛰기.py
+++ Programmers/멀리 뛰기.py
@@ -1,9 +1,11 @@
 def solution(n):
-    if n in (1, 2):
-        return n
-    return solution(n-1) + solution(n-2)
+    tmp = [1,2]
+    for i in range(2, n):
+        tmp.append(tmp[i-2] + tmp[i-1])
 
-n = [4, 3]
+    return tmp[n-1]%1234567
+
+n = [1, 3]
 
 print(solution(n[0]))
 print(solution(n[1]))
