63ff6e0fe2a758887fefdfbdef1255c4c5e44d0f
hong8yung <hoyun342@gmail.com>
2019-09-11 23:14:21 +0900

    Programmers_문자열을 정수로 바꾸기

--- Programmers/문자열을 정수로 바꾸기.py
+++ Programmers/문자열을 정수로 바꾸기.py
@@ -1,7 +1,20 @@
 def solution(s):
+    result = 0
+    for idx, i in enumerate(s[::-1]):
+        if i == '-':
+            result *= -1
+        elif i == '+':
+            pass
+        else:
+            result += (ord(i) - ord('0'))*(10**idx)
+
+    return result
+
+def old_solution(s):
     return int(s)
 
-strTmp = ["1234", "-1234"]
+strTmp = ["1234", "-1234", "+1234"]
 
 print(solution(strTmp[0]))
 print(solution(strTmp[1]))
+print(solution(strTmp[2]))
