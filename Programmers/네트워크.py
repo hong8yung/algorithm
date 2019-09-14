a53296d161c6df3dea8d5ddaa151401ccf6e1219
hong8yung <hoyun342@gmail.com>
2019-09-14 09:47:06 +0900

    Programmers_네트워크

--- Programmers/네트워크.py
+++ Programmers/네트워크.py
@@ -1,5 +1,26 @@
-vect = []
 def solution(n, computers):
+    answer = 0
+    visited = [0 for i in range(n)]
+    def dfs(computers, visited, start):
+        stack  = [start]
+        while stack:
+            j = stack.pop()
+            if visited[j] == 0:
+                visited[j] = 1
+
+            for i in range(0, len(computers)):
+                if computers[j][i] == 1 and visited[i] == 0:
+                    stack.append(i)
+
+    i = 0
+    while 0 in visited:
+        if visited[i] == 0:
+            dfs(computers, visited, i)
+            answer += 1
+        i += 1
+    return answer
+
+def old_solution(n, computers):
     network = []
     for i in range(n):
         network.append({i})
@@ -7,7 +28,6 @@ def solution(n, computers):
     for i in range(n):
         for j in range(i+1, n):
             if computers[i][j] == 1:
-                print(i, j, network)
                 for idx, com in enumerate(network):
                     if i in com:
                         idx_1 = idx
