f2b672ed7afb438d9b517293aea7dfd1f5456697
hong8yung <hoyun342@gmail.com>
2019-09-21 19:27:25 +0900

    Programmers_하샤드 수

--- Programmers/하샤드 수.py
+++ Programmers/하샤드 수.py
@@ -5,8 +5,24 @@ def solution(x):
 
     return x%tmp_sum==0
 
+def row_solution(x):
+    tmp_sum = 0
+    tmp_num = x
+    while tmp_num:
+        tmp_sum += tmp_num%10
+        tmp_num //= 10
+
+    return x%tmp_sum is 0
+    
+
+
 arr = [10, 12, 11, 13]
 print(solution(arr[0]))
 print(solution(arr[1]))
 print(solution(arr[2]))
 print(solution(arr[3]))
+
+print(row_solution(arr[0]))
+print(row_solution(arr[1]))
+print(row_solution(arr[2]))
+print(row_solution(arr[3]))
