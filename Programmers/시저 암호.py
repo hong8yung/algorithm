e17cde902c7529c169ea8e1520effd83c6eb86e8
hong8yung <hoyun342@gmail.com>
2019-10-15 03:26:32 +0900

    Programmers_시저암호

--- Programmers/시저 암호.py
+++ Programmers/시저 암호.py
@@ -1,3 +1,14 @@
+def idel_solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+
+    return "".join(s)
+
 def solution(s, n):
     answer = ''
     k = ord('z') - ord('a') + 1
